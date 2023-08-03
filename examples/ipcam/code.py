adb0443290afa61c20ce8645fa4d46b5ae21817e
Jeff Epler <jepler@gmail.com>
2023-08-03 14:16:04 -0500

    add an IP-camera demo

---
+++ examples/ipcam/code.py
@@ -0,0 +1,67 @@
+import asyncio
+import binascii
+import os
+import struct
+import sys
+import time
+
+import adafruit_pycamera
+import espcamera
+import socketpool
+import wifi
+from adafruit_httpserver.response import ChunkedResponse
+from adafruit_httpserver.server import Server
+
+pycam = adafruit_pycamera.PyCamera()
+pycam.camera.reconfigure(
+    pixel_format=espcamera.PixelFormat.JPEG,
+    frame_size=espcamera.FrameSize.SVGA,
+)
+pycam.camera.quality = 6
+
+server = Server(socketpool.SocketPool(wifi.radio))
+PORT = 81
+BOUNDARY = b"FRAME" + binascii.hexlify(os.urandom(8))
+
+
+@server.route("/")
+def base(request):
+    def body():
+        while True:
+            jpeg = pycam.camera.take()
+            yield b"--"
+            yield BOUNDARY
+            yield b"\r\n"
+            yield b"Content-Type: image/jpeg\r\nContent-Length: "
+            yield str(len(jpeg))
+            yield "\r\n\r\n"
+            yield jpeg
+            yield "\r\n"
+
+    return ChunkedResponse(
+        request,
+        body,
+        headers={
+            "Content-Type": "multipart/x-mixed-replace; boundary=%s"
+            % BOUNDARY.decode("ascii")
+        },
+    )
+
+
+async def poll(interval):
+    server.start(str(wifi.radio.ipv4_address), port=PORT)
+    while True:
+        try:
+            server.poll()
+        except BrokenPipeError as e:
+            print(e)
+        await asyncio.sleep(interval)
+
+
+async def main():
+    poll_task = asyncio.create_task(poll(0))
+    await asyncio.gather(poll_task)
+
+pycam.display_message(f"{wifi.radio.ipv4_address}:{PORT}/", scale=2)
+
+asyncio.run(main())
