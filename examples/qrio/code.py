71e3653f3b9919dafdb546f86dd9bb345f55df3c
Jeff Epler <jepler@gmail.com>
2023-08-07 09:50:56 -0500

    add a qr-scanning ap

---
+++ examples/qrio/code.py
@@ -0,0 +1,51 @@
+# SPDX-FileCopyrightText: Copyright (c) 2021 Jeff Epler for Adafruit Industries
+#
+# SPDX-License-Identifier: Unlicense
+
+"""
+This demo is designed for the Kaluga development kit version 1.3 with the
+ILI9341 display.
+"""
+
+from terminalio import FONT
+import board
+import busio
+import displayio
+import qrio
+import time
+import bitmaptools
+import espcamera
+from adafruit_display_text.bitmap_label import Label
+from adafruit_pycamera import PyCamera
+
+zoomed = displayio.Bitmap(240, 176, 65535)
+pycam = PyCamera()
+pycam.camera.reconfigure(
+    pixel_format=espcamera.PixelFormat.RGB565,
+    frame_size=espcamera.FrameSize.VGA,
+    )
+pycam._mode_label.text = "QR SCAN"
+pycam._res_label.text = ""
+pycam.effect = 0
+pycam.display.refresh()
+qrdecoder = qrio.QRDecoder(zoomed.width, zoomed.height)
+
+old_payload = None
+while True:
+    new_frame = pycam.continuous_capture()
+    if new_frame is None:
+        continue
+    bitmaptools.blit(zoomed, new_frame, 0, 0, x1=(640-240)//2, y1=(480-176)//2)
+    pycam.blit(zoomed)
+    for row in qrdecoder.decode(zoomed, qrio.PixelPolicy.RGB565_SWAPPED):
+        payload = row.payload
+        try:
+            payload = payload.decode("utf-8")
+        except UnicodeError:
+            payload = str(payload)
+        if payload != old_payload:
+            pycam.tone(200, 0.1)
+            print(payload)
+            pycam.display_message(payload, color=0xffffff, scale=1)
+            time.sleep(1)
+            old_payload = payload
