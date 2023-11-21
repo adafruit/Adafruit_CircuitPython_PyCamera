3bcc927a781425744252984018d1d544645a9c4d
Jeff Epler <jepler@gmail.com>
2023-11-21 17:16:12 -0600

    stating new ipcam example

---
+++ examples/ipcam2/code.py
@@ -0,0 +1,212 @@
+import espcamera
+import os
+import sys
+import time
+import struct
+import board
+import adafruit_pycamera
+import displayio
+import gifio
+import ulab.numpy as np
+import bitmaptools
+import socketpool
+import wifi
+from adafruit_httpserver import Server, Request, Response, GET, POST
+
+pycam = adafruit_pycamera.PyCamera()
+pycam.autofocus_init()
+
+if wifi.radio.ipv4_address:
+    # use alt port if web workflow enabled
+    port = 8080
+else:
+    # connect to wifi and use standard http port otherwise
+    wifi.radio.connect(os.getenv("WIFI_SSID"), os.getenv("WIFI_PASSWORD"))
+    port = 80
+
+print(wifi.radio.ipv4_address)
+
+pool = socketpool.SocketPool(wifi.radio)
+server = Server(pool, debug=True)
+
+@server.route("/lcd", [GET, POST])
+def property(request: Request) -> Response:
+    pycam.blit(pycam.continuous_capture())
+    return Response(request, "")
+
+@server.route("/jpeg", [GET, POST])
+def property(request: Request) -> Response:
+    pycam.camera.reconfigure(
+        pixel_format=espcamera.PixelFormat.JPEG,
+        frame_size=pycam.resolution_to_frame_size[pycam._resolution]
+    )
+    try:
+        jpeg = pycam.camera.take(1)
+        if jpeg is not None:
+            return Response(request, bytes(jpeg), content_type="image/jpeg")
+        else:
+            return Response(request, "", content_type="text/plain", status=INTERNAL_SERVER_ERROR_500)
+    finally:
+        pycam.live_preview_mode()
+
+@server.route("/property", [GET, POST])
+def property(request: Request) -> Response:
+    enctype = request.query_params.get("enctype", "text/plain")
+
+    key = request.form_data.get("k")
+    value = request.form_data.get("v", None)
+        
+    try:
+        current_value = getattr(pycam, key, None)
+    except Exception as e:
+        return Response(request, str(e), status=NOT_FOUND_404)
+
+    if value is None:
+        return Response(request, str(current_value))
+    else:
+        try:
+            current_value_type = type(current_value)
+            if current_value_type is bool:
+                setattr(pycam, key, value not in ('False', '0', 'no', 'n', 'off', ''))
+            else:
+                setattr(pycam, key, current_value_type(value))
+            return Response(request, "")
+        except Exception as e:
+            return Response(request, str(e), status=BAD_REQUEST_400)
+
+server.serve_forever(str(wifi.radio.ipv4_address), port)
+
+
+server = Server(pool, debug=True)
+
+last_frame = displayio.Bitmap(pycam.camera.width, pycam.camera.height, 65535)
+onionskin = displayio.Bitmap(pycam.camera.width, pycam.camera.height, 65535)
+while True:
+    if  (pycam.mode_text == "STOP" and pycam.stop_motion_frame != 0):
+        # alpha blend
+        new_frame = pycam.continuous_capture()
+        bitmaptools.alphablend(onionskin, last_frame, new_frame,
+                               displayio.Colorspace.RGB565_SWAPPED)
+        pycam.blit(onionskin)
+    else:
+        pycam.blit(pycam.continuous_capture())
+    #print("\t\t", capture_time, blit_time)
+
+    pycam.keys_debounce()
+    print(f"{pycam.shutter.released=} {pycam.shutter.long_press=} {pycam.shutter.short_count=}")
+    # test shutter button
+    if pycam.shutter.long_press:
+        print("FOCUS")
+        print(pycam.autofocus_status)
+        pycam.autofocus()
+        print(pycam.autofocus_status)
+    if pycam.shutter.short_count:
+        print("Shutter released")
+        if pycam.mode_text == "STOP":
+            pycam.capture_into_bitmap(last_frame)
+            pycam.stop_motion_frame += 1
+            try:
+                pycam.display_message("Snap!", color=0x0000FF)
+                pycam.capture_jpeg()
+            except TypeError as e:
+                pycam.display_message("Failed", color=0xFF0000)
+                time.sleep(0.5)
+            except RuntimeError as e:
+                pycam.display_message("Error\nNo SD Card", color=0xFF0000)
+                time.sleep(0.5)
+            pycam.live_preview_mode()
+
+        if pycam.mode_text == "GIF":
+            try:
+               f = pycam.open_next_image("gif")
+            except RuntimeError as e:
+               pycam.display_message("Error\nNo SD Card", color=0xFF0000)
+               time.sleep(0.5)
+               continue
+            i = 0
+            ft = []
+            pycam._mode_label.text = "RECORDING"
+
+            pycam.display.refresh()
+            with gifio.GifWriter(f, pycam.camera.width, pycam.camera.height,
+                                 displayio.Colorspace.RGB565_SWAPPED, dither=True) as g:
+                t00 = t0 = time.monotonic()
+                while (i < 15) or (pycam.shutter_button.value == False):
+                    i += 1
+                    _gifframe = pycam.continuous_capture()
+                    g.add_frame(_gifframe, .12)
+                    pycam.blit(_gifframe)
+                    t1 = time.monotonic()
+                    ft.append(1/(t1-t0))
+                    print(end=".")
+                    t0 = t1
+            pycam._mode_label.text = "GIF"
+            print(f"\nfinal size {f.tell()} for {i} frames")
+            print(f"average framerate {i/(t1-t00)}fps")
+            print(f"best {max(ft)} worst {min(ft)} std. deviation {np.std(ft)}")
+            f.close()
+            pycam.display.refresh()
+
+        if pycam.mode_text == "JPEG":
+            pycam.tone(200, 0.1)
+            try:
+                pycam.display_message("Snap!", color=0x0000FF)
+                pycam.capture_jpeg()
+                pycam.live_preview_mode()
+            except TypeError as e:
+                pycam.display_message("Failed", color=0xFF0000)
+                time.sleep(0.5)
+                pycam.live_preview_mode()
+            except RuntimeError as e:
+                pycam.display_message("Error\nNo SD Card", color=0xFF0000)
+                time.sleep(0.5)
+    if pycam.card_detect.fell:
+        print("SD card removed")
+        pycam.unmount_sd_card()
+        pycam.display.refresh()
+    if pycam.card_detect.rose:
+        print("SD card inserted")
+        pycam.display_message("Mounting\nSD Card", color=0xFFFFFF)
+        for _ in range(3):
+            try:
+                print("Mounting card")
+                pycam.mount_sd_card()
+                print("Success!")
+                break
+            except OSError as e:
+                print("Retrying!", e)
+                time.sleep(0.5)
+        else:
+            pycam.display_message("SD Card\nFailed!", color=0xFF0000)
+            time.sleep(0.5)
+        pycam.display.refresh()
+
+    if pycam.up.fell:
+        print("UP")
+        key = settings[curr_setting]
+        if key:
+            setattr(pycam, key, getattr(pycam, key) + 1)
+    if pycam.down.fell:
+        print("DN")
+        key = settings[curr_setting]
+        if key:
+            setattr(pycam, key, getattr(pycam, key) - 1)
+    if pycam.left.fell:
+        print("LF")
+        curr_setting = (curr_setting + 1) % len(settings)
+        print(settings[curr_setting])
+        #new_res = min(len(pycam.resolutions)-1, pycam.get_resolution()+1)
+        #pycam.set_resolution(pycam.resolutions[new_res])
+        pycam.select_setting(settings[curr_setting])
+    if pycam.right.fell:
+        print("RT")
+        curr_setting = (curr_setting - 1 + len(settings)) % len(settings)
+        print(settings[curr_setting])
+        pycam.select_setting(settings[curr_setting])
+        #new_res = max(1, pycam.get_resolution()-1)
+        #pycam.set_resolution(pycam.resolutions[new_res])
+    if pycam.select.fell:
+        print("SEL")
+    if pycam.ok.fell:
+        print("OK")
+
