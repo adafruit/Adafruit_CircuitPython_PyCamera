bf5559c3d3dc13c00187328cf4195af3c21b1e5d
Fred Meyer <fred@meyer.org>
2024-03-17 18:13:18 -0700

    Correct Another DOS line ending

--- examples/camera/code.py
+++ examples/camera/code.py
@@ -29,7 +29,7 @@ TZ = os.getenv("TZ")
 print(f"Connecting to {os.getenv('CIRCUITPY_WIFI_SSID')}")
 wifi.radio.connect(
     os.getenv("CIRCUITPY_WIFI_SSID"), os.getenv("CIRCUITPY_WIFI_PASSWORD")
-    )
+)
 if wifi.radio.connected:
     print(f"Connected to {os.getenv('CIRCUITPY_WIFI_SSID')}!")
     print("My IP address is", wifi.radio.ipv4_address)
